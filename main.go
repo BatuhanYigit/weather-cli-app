d09666a8d199869e45d4abd07c3394b30cd176da
BatuhanYigit <batuhanyigit34@gmail.com>
2024-01-15 17:15:45 +0300

    Add Location input
    Add Location input

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
 )
 
 type Weather struct {
 	Location struct {
 		Name    string `json:"name"`
 		Country string `json:"country"`
-	} `json:location`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64   `json:"time_epoch"`
+				TempC     float64 `json:"temp_c"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
 }
 
 func main() {
-	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=a7bf715a8d8f401f9fe123814241501&q=Istanbul&days=1&aqi=no&alerts=no")
+	q := "Ankara"
+	if len(os.Args) >= 2 {
+		q = os.Args[1]
+	}
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=a7bf715a8d8f401f9fe123814241501&q=" + q + "&days=1&aqi=no&alerts=no")
 
 	if err != nil {
 		panic(err)
@@ -30,5 +57,40 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(string(body))
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+
+	fmt.Printf("%s,%s: %.0fC,%s\n",
+		location.Name,
+		location.Country,
+		current.TempC,
+		current.Condition.Text)
+
+	for _, hour := range hours {
+		date := time.Unix(hour.TimeEpoch, 0)
+
+		if date.Before(time.Now()) {
+			continue
+		}
+
+		message := fmt.Sprintf("%s - %.0fC, %.0f%%,%s\n",
+			date.Format("15:04"),
+			hour.TempC,
+			hour.ChanceOfRain,
+			hour.Condition.Text,
+		)
+
+		if hour.ChanceOfRain < 40 {
+			fmt.Print(message)
+		} else {
+			color.Red(message)
+
+		}
+
+	}
 }
