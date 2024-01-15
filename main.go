d11b51ee3684a24915bf4ed464cce8a5062067fc
BatuhanYigit <batuhanyigit34@gmail.com>
2024-01-15 15:59:12 +0300

    Add Api connect
    *Add response
    *Add Request api
    *Add api key

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
-type Config struct {
-	APIKey string `json:"api_key"`
-}
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
-type WeatherData struct {
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:location`
 }
 
 func main() {
+	res, err := http.Get("http://api.weatherapi.com/v1/forecast.json?key=a7bf715a8d8f401f9fe123814241501&q=Istanbul&days=1&aqi=no&alerts=no")
 
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather api error")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
